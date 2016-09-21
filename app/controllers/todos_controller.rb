c0104e22cd4ca942f71668fe0b40bd8c99362b36
Beth Givens <eag0013@gmail.com>
2016-09-21 17:26:38 +0000

    Finished through step 7.3 successfully.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,2 @@
 class TodosController < ApplicationController
-
-  def index
-    @todos = Todo.all
-  end
-
-  def create
-    @todo = Todo.create(todo_params)
-    redirect_to '/todo'
-  end
-
-  private
-
-  def todo_params
-    params.require(:todo).permit(:item)
-  end
-  
 end
