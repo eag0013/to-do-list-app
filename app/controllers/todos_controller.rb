d1c60f6b44e882f9c5387451d7787eea86a20136
Beth Givens <eag0013@gmail.com>
2016-09-29 02:29:37 +0000

    Added edit and delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,39 @@
 class TodosController < ApplicationController
+    
+def index
+  @todos = Todo.all
+end
+
+def new
+  @todo = Todo.new
+end
+
+def create
+  @todo = Todo.create(todo_params)
+  redirect_to todos_path
+end
+
+private # why does it say private? what happens if this changes?
+def todo_params
+  params.require(:todo).permit(:item)
+end
+
+def edit
+  @todo = Todo.find(params[:id])
+  @todo.update(todo_params)
+  redirect_to todos_path
+end
+
+def update
+  @todo = Todo.find(params[:id]) 
+  @todo.update
+  redirect_to todos_path
+end
+
+def destroy
+  @todo = Todo.find(params[:id])
+  @todo.destroy
+  redirect_to todos_path
+end
+
 end
